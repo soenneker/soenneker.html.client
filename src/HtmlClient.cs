6a532072e441c7047678b4b3d79add00b237afb9
Jake Soenneker <jake@soenneker.com>
2026-01-06 19:49:26 -0600

    org

--- src/HtmlClient.cs
+++ src/HtmlClient.cs
@@ -2,7 +2,6 @@ using Soenneker.Html.Client.Abstract;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-using System;
 using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Html.Client;
@@ -12,6 +11,8 @@ public sealed class HtmlClient : IHtmlClient
 {
     private readonly IHttpClientCache _httpClientCache;
 
+    private const string _cacheKey = nameof(HtmlClient);
+
     public HtmlClient(IHttpClientCache httpClientCache)
     {
         _httpClientCache = httpClientCache;
@@ -19,16 +20,16 @@ public sealed class HtmlClient : IHtmlClient
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
-        return _httpClientCache.Get(nameof(HtmlClient), cancellationToken: cancellationToken);
+        return _httpClientCache.Get(_cacheKey, cancellationToken: cancellationToken);
     }
 
     public void Dispose()
     {
-        _httpClientCache.RemoveSync(nameof(HtmlClient));
+        _httpClientCache.RemoveSync(_cacheKey);
     }
 
     public ValueTask DisposeAsync()
     {
-        return _httpClientCache.Remove(nameof(HtmlClient));
+        return _httpClientCache.Remove(_cacheKey);
     }
 }
